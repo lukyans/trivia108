ec48294ec5d7af16bbf6868dbc6e289f4559854d
Sergey <sukyans@gmail.com>
2020-04-13 11:16:32 -0600

    add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when 'info' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+      when 'email' then "alert alert-warning"
+      when 'password' then "alert alert-warning"
+    end
+  end
 end
