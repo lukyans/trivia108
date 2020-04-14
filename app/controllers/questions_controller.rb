7486a15349683bb99a1990f3cb5ea81b48e85a1c
Sergey <sukyans@gmail.com>
2020-04-14 12:49:51 -0600

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -44,7 +44,7 @@ class QuestionsController < ApplicationController
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.html { redirect_to questions_path, notice: 'Question was successfully updated.' }
         format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :edit }
