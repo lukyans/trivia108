c1318ed86791a24dfe88f8cc40a9739910becfaa
Sergey <sukyans@gmail.com>
2020-04-14 13:59:24 -0600

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,7 +34,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to questions_path, notice: 'Question was successfully created.' }
+        format.html { redirect_to admin_questions_path, notice: 'Question was successfully created.' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
@@ -48,7 +48,7 @@ class QuestionsController < ApplicationController
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to questions_path, notice: 'Question was successfully updated.' }
+        format.html { redirect_to admin_questions_path, notice: 'Question was successfully updated.' }
         format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :edit }
