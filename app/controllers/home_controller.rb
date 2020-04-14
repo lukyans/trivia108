cf93fbdda68753bec472230ec5bb2a23af66f1ac
Sergey <sukyans@gmail.com>
2020-04-14 11:53:16 -0600

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  # before_action :set_question
+
   def index
+    # binding.pry
+    @answer = Question.all
   end
+
+  # private
+    # Use callbacks to share common setup or constraints between actions.
+    # def set_question
+    #   @question = Question.find(params[:question_id])
+    # end
+    #
 end
