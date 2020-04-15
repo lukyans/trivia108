404153eaf9d1cd6788b2d3ce287bb777b6660d3e
Sergey <sukyans@gmail.com>
2020-04-14 21:04:56 -0600

    add fake data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  50.times do
+    User.create!(email: Faker::Internet.email, password: Faker::Internet.password, points: rand(1..1000))
+  end
+
+  100.times do
+    q = Question.create!(name: Faker::Food.vegetables, user_id: User.all.sample.id, created_at: Faker::Date)
+    Answer.create!(question_id: q.id, name: "vegetable", correct: true)
+    Answer.create!(question_id: q.id, name: "dish", correct: false)
+    Answer.create!(question_id: q.id, name: "fruit", correct: false)
+  end
+
+  100.times do
+    q = Question.create!(name: Faker::Food.dish, user_id: User.all.sample.id, created_at: Faker::Date)
+    Answer.create!(question_id: q.id, name: "dish", correct: true)
+    Answer.create!(question_id: q.id, name: "fruit", correct: false)
+    Answer.create!(question_id: q.id, name: "vegetable", correct: false)
+  end
+
+  100.times do
+    q = Question.create!(name: Faker::Food.fruits, user_id: User.all.sample.id, created_at: Faker::Date)
+    Answer.create!(question_id: q.id, name: "fruit", correct: true)
+    Answer.create!(question_id: q.id, name: "dish", correct: false)
+    Answer.create!(question_id: q.id, name: "vegetable", correct: false)
+  end
