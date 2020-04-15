59ca97a45448e9089acd887daaef637fb1329924
Sergey <sukyans@gmail.com>
2020-04-14 23:37:47 -0600

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @top_users = User.order("points DESC")
+    @average_score = User.average_score
+    @players_count = User.count
   end
 end
